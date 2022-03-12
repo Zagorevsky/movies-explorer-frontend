62681d283d1309b5f962b1be7ed795b1120d4219
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-03-12 17:10:21 +0300

    feat: NavTab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -4,10 +4,10 @@ import './NavTab.css';
 function NavTab() {
   return (
     <nav className="nav-tab">
-      <ul className="footer-menu">
-        <li><a className="" href="/">О проекте</a></li>
-        <li><a className="" href="/">Технологии</a></li>
-        <li><a className="" href="/">Студент</a></li>
+      <ul className="nav-tab__menu">
+        <li><a className="nav-tab__link" href="/">О проекте</a></li>
+        <li><a className="nav-tab__link" href="/">Технологии</a></li>
+        <li><a className="nav-tab__link" href="/">Студент</a></li>
       </ul>
     </nav>
   );
