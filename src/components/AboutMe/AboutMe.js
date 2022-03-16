81c771d483fdc405bd35a48d07431b444034718c
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-03-16 19:52:55 +0300

    feat: AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,7 +3,30 @@ import './AboutMe.css';
 
 function AboutMe() {
   return (
-    <div className="about-me">Студент</div>
+    <div className="about-me">
+      <div className="about-me__container">
+        <h2 className="about-me__title">Студент</h2>
+        <ul className="about-me__tab">
+          <li className="about-me__block">
+            <h3 className="about-me__name">Виталий</h3>
+            <p className="about-me__profil">Фронтенд-разработчик, 30 лет</p>
+            <p className="about-me__info">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+              У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
+              С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
+              начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+            <nav>
+              <ul className="about-me__menu">
+                <li><a className="about-me__link" href="https://github.com">Github</a></li>
+                <li><a className="about-me__link" href="https://www.facebook.com">Facebook</a></li>
+              </ul>
+            </nav>
+          </li>
+          <li className="about-me__block about-me__block_foto">
+            <div className="about-me__foto"/>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 
 };
