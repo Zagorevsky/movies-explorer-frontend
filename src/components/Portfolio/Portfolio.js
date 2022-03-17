740bfde294fc63d1e166653776cecb1628daf051
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-03-17 12:34:21 +0300

    feat: Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,18 @@ import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <div className="portfolio">Портфолио</div>
+    <portfolio className="portfolio">
+      <div className="portfolio__container">
+        <p className="portfolio__title">Портфолио</p>
+        <nav className="portfolio__block">
+          <ul className="portfolio__menu">
+            <li><a className="portfolio__menu-link" href="/"><div className="portfolio__menu-block">Статичный сайт</div></a></li>
+            <li><a className="portfolio__menu-link" href="/"><div className="portfolio__menu-block">Адаптивный сайт</div></a></li>
+            <li><a className="portfolio__menu-link" href="/"><div className="portfolio__menu-block">Одностраничное приложение</div></a></li>
+          </ul>
+        </nav>
+      </div>
+    </portfolio>
   );
 
 };
